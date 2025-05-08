4b822c98107b07c0963aa3066f73a0b1e928c9f2
Denis Sotnikov <sotnikovdenis04@gmail.com>
2025-05-08 14:25:28 +0500

    Project structure changed

--- src/user/service.py
+++ src/user/service.py
@@ -1,11 +1,9 @@
 ﻿from pydantic import UUID4
-from sqlalchemy.orm import Session
 
 from src.user.schemas import User as UserDTO
 
 from src.user.schemas import NewUser, User
 
-db_session = Session
 
 async def register_new_user(*, user: NewUser) -> UserDTO:
     pass
